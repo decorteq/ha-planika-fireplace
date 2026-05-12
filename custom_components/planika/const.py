7a398db6083d5ae50f37f38c5bebbbe0358d741f
decorteq <72691289+decorteq@users.noreply.github.com>
2026-05-12 23:41:10 +0200

    Create const.py

---
+++ custom_components/planika/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Planika Fireplace integration."""
+
+DOMAIN = "planika"
+DEFAULT_NAME = "Planika Fireplace"
+DEFAULT_PORT = 3000
+
+CONF_FLAME_LEVEL = "flame_level"
+
+# Polling interval in seconds
+SCAN_INTERVAL_SECONDS = 30
